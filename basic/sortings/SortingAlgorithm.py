4a57327bd268509543036bb8a9a516cf24f35be8
= <=>
2013-01-14 22:54:27 +0800

    add bubble sort and merge sort

---
+++ basic/sortings/SortingAlgorithm.py
@@ -0,0 +1,81 @@
+'''
+Created on 2013-1-14
+
+@author: stefanie
+'''
+import copy
+
+class SortingAlgorithm(object):
+    
+    def __init__(self, array = range(1,10)):
+        '''
+        Constructor
+        '''
+        self.array = array
+    
+    def bottom_bubble(self):
+        array = copy.copy(self.array)
+        for i in range(len(array)):
+            for j in range(i, len(array)):
+                if array[i] > array[j]:
+                    array[i], array[j] = array[j], array[i]
+        return array
+                    
+    def top_bubble(self):
+        array = copy.copy(self.array)
+        for i in range(1, len(array)):
+            for j in range(0, i):
+                if array[i] < array[j]:
+                    array[i], array[j] = array[j], array[i]
+        return array;
+    
+    def quick_sorting(self):
+        array = copy.copy(self.array)
+    
+    def division_merge(self):
+        array = copy.copy(self.array)
+        self._division(array, 0, len(array) -1)
+        return array
+    
+    def _division(self, array, begin, end):
+        if begin < end:
+            index = (begin + end) / 2
+            self._division(array, begin, index)
+            self._division(array, index + 1, end)
+            self._merge(array, begin, index, end)
+        
+    def _merge(self, array, begin, index, end):
+#        print begin, index, end
+        result = range(0, end-begin+1)
+        i = begin; j = index + 1; k = 0
+        while i <= index and j <= end:
+            if array[i] < array[j]:
+                #print k, i
+                result[k] = array[i]
+                i += 1
+            else :
+                result[k] = array[j]
+                j += 1
+            k += 1
+        while i <= index:
+            result[k] = array[i]
+            k += 1; i += 1
+        while j <= end:
+            result[k] = array[j]
+            k += 1; j += 1
+        #print array, result
+        array[begin:end + 1] = result
+        #print array
+                       
+#main function
+if __name__ == '__main__':
+    #test_split_balance_array()
+    array = [2,34,4,23,67,1,7,19]
+    
+    print array    
+    sorting = SortingAlgorithm(array)
+   
+    print "Top Bubble Result: ", sorting.top_bubble()
+    print "Button Bubble Result: ", sorting.bottom_bubble()
+    print "Merge Sorting Result: ", sorting.division_merge()
+    
