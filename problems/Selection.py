654e8c4c26cab385092a53301c8b192df01461c0
= <=>
2013-03-15 17:37:42 +0800

    add a problem find kth largest number

---
+++ problems/Selection.py
@@ -0,0 +1,44 @@
+'''
+Created on 2013-3-15
+
+@author: stefanie
+'''
+
+'''
+Problem: given a array, find the Kth largest number in the array
+Solution: 
+    using QuickSort main idea, to partition the array into two part, and recursively find the Kth number
+'''
+
+def selection(array, k):
+    return select(array, 0, len(array) -1, k);
+    
+def select(array, low, high, k):
+    mid = partition(array, low, high)
+    index = high - k + 1
+    if mid == index:
+        return array[mid]
+    elif mid < index:
+        return select(array, mid+1, high, k)
+    else:
+        return select(array, low, mid-1, k-(high-mid+1))
+       
+
+def partition(array, low, high):
+        key = array[high]
+        j = low
+        i = low -1
+        while j < high:
+            if array[j] < key:
+                i += 1
+                array[i], array[j] = array[j], array[i]
+            j += 1
+        i += 1
+        array[high], array[i] = array[i], array[high]
+        return i 
+
+if __name__ == '__main__':
+    array = [79, 82, 23, 50, 17, 53, 37, 89, 60, 14]
+    print array
+    for i in range(1,len(array)+1)[::-1]:
+        print i,'th num:', selection(array, i);
